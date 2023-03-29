56aa9ae1f6bac735dc10079f7e38fb4aafe8e7d6
Etienne Tourigny <etienne.tourigny@bsc.es>
2023-03-29 13:35:03 +0000

    workaround for merge does not work well with Argument Groups

--- preprocess_inputdata_month.sh
+++ preprocess_inputdata_month.sh
@@ -15,6 +15,8 @@
 # M. Athanase, H. Goessling
 ######################################################################## 
 
+set -xuve
+
 CDO=$1 		    # path to CDO module
 SCRIPTPATH=$2 	    # path to script folder
 POOL=$3  	    # path to raw reanalysis data
@@ -31,8 +33,17 @@ echo 'monthly pre-processing input files, start preprocess_inputdata_month.sh'
 
    # Preparation of spectral input files
 
-eval ${CDO} -P 8 -merge [ -mergetime -apply,-seltimestep,1/24/6 [ ${POOL}/${YEAR}/*_1H_${YEAR}-${MONTH}-*_129 ] ] [ -mergetime -apply,-seltimestep,1/24/6 [ ${POOL}/${YEAR}/*_1H_${YEAR}-${MONTH}-*_152 ] ] [ -mergetime -apply,-seltimestep,1/24/6 [ ${POOL}/${YEAR}/*_1H_${YEAR}-${MONTH}-*_130 ] ] [ -mergetime -apply,-seltimestep,1/24/6 [ ${POOL}/${YEAR}/*_1H_${YEAR}-${MONTH}-*_138 ] ] [ -mergetime -apply,-seltimestep,1/24/6 [ ${POOL}/${YEAR}/*_1H_${YEAR}-${MONTH}-*_155 ] ] ${NDGPATH}/${NDGTAG}${YEAR}${MONTH}.sp
+#eval ${CDO} -P 8 -merge [ -mergetime -apply,-seltimestep,1/24/6 [ ${POOL}/${YEAR}/*_1H_${YEAR}-${MONTH}-*_129 ] ] [ -mergetime -apply,-seltimestep,1/24/6 [ ${POOL}/${YEAR}/*_1H_${YEAR}-${MONTH}-*_152 ] ] [ -mergetime -apply,-seltimestep,1/24/6 [ ${POOL}/${YEAR}/*_1H_${YEAR}-${MONTH}-*_130 ] ] [ -mergetime -apply,-seltimestep,1/24/6 [ ${POOL}/${YEAR}/*_1H_${YEAR}-${MONTH}-*_138 ] ] [ -mergetime -apply,-seltimestep,1/24/6 [ ${POOL}/${YEAR}/*_1H_${YEAR}-${MONTH}-*_155 ] ] ${NDGPATH}/${NDGTAG}${YEAR}${MONTH}.sp
+
+# workaround for "merge does not work well with Argument Groups"
+# https://code.mpimet.mpg.de/boards/1/topics/14213
+rm -f ${NDGPATH}/${NDGTAG}${YEAR}${MONTH}-*.sp ${NDGPATH}/${NDGTAG}${YEAR}${MONTH}.sp
+for v in 129 152 130 138 155 ; do
+    eval ${CDO} -O -P 8 -mergetime -apply,-seltimestep,1/24/6 [ ${POOL}/${YEAR}/*_1H_${YEAR}-${MONTH}-*_${v} ] ${NDGPATH}/${NDGTAG}${YEAR}${MONTH}-${v}.sp
+done
+eval ${CDO} -O -P 8 -merge ${NDGPATH}/${NDGTAG}${YEAR}${MONTH}-*.sp ${NDGPATH}/${NDGTAG}${YEAR}${MONTH}.sp
+rm -f ${NDGPATH}/${NDGTAG}${YEAR}${MONTH}-*.sp
 
    # Preparation of grid-point input files
-eval ${CDO} -P 8 -mergetime -apply,-seltimestep,1/24/6 [ ${POOL}/${YEAR}/*_1H_${YEAR}-${MONTH}-*_133 ] ${NDGPATH}/${NDGTAG}${YEAR}${MONTH}.gp 
+eval ${CDO} -O -P 8 -mergetime -apply,-seltimestep,1/24/6 [ ${POOL}/${YEAR}/*_1H_${YEAR}-${MONTH}-*_133 ] ${NDGPATH}/${NDGTAG}${YEAR}${MONTH}.gp 
 
